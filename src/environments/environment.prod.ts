34ded3eeaa14a8fc31b80578e233a4033b019965
stevewitman <stevewitman@gmail.com>
2018-07-30 10:16:16 -0600

    Changed sent notes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAWeEdfUqeWrrxxS1oU-r3Kk8XNuSAjwgQ",
+    authDomain: "angularcolorado.firebaseapp.com",
+    databaseURL: "https://angularcolorado.firebaseio.com",
+    projectId: "angularcolorado",
+    storageBucket: "angularcolorado.appspot.com",
+    messagingSenderId: "606878939230"
+  }
 };
